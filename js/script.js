8f5ee293a07e5768ada1ebde0ff74f4d6480d214
tamaacuek <ociganteng81@gmail.com>
2024-10-30 15:19:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+let previewContainer = document.querySelector('.jasa-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.row .jasa-card').forEach(product =>{
+    product.onclick = () =>{
+        previewContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+        previewBox.forEach(preview =>{
+            let target = preview.getAttribute('data-target');
+            if(name == target){
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close =>{
+   close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    previewContainer.style.display = 'none';
+   }; 
+});
